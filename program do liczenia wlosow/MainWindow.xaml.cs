b138a514dbd045cb0092fe63d728f2cee67fc493
patryksz18 <patrykkarol07@gmail.com>
2024-10-31 19:30:18 +0100

    polaczenie programu do aplikcajio z klasa

--- program do liczenia wlosow/MainWindow.xaml.cs
+++ program do liczenia wlosow/MainWindow.xaml.cs
@@ -1,24 +1,12 @@
-﻿using System.Text;
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace program_do_liczenia_wlosow
+namespace SzacowanieWlosowNaGlowie
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        private Glowa glowa = new Glowa();
+
+       
     }
 }
